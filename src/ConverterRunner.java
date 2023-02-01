eef03dfce139b6d598266d212da2ad58d110df67
jianx <jxu5406@bths.edu>
2023-01-31 19:41:38 -0500

    Patchnotes v1.01
    Idiot proofing
    
    I hope that's all the test cases

--- src/ConverterRunner.java
+++ src/ConverterRunner.java
@@ -8,16 +8,42 @@ class ConverterRunner {
         System.out.print("Enter the base of your number (2, 8 or 10): ");
 
         Scanner s = new Scanner(System.in);
-        String choice = s.nextLine();
-        int base = Integer.parseInt(choice);
+        String choice = "";
+        int base = 0;
+        for (int i = 0; i < 1; i++) {
+            choice = s.nextLine();
+            if (choice.equalsIgnoreCase("2") || choice.equalsIgnoreCase("8") || choice.equalsIgnoreCase("10")){
+                base = Integer.parseInt(choice);
+            } else {
+                System.out.println("That's not valid!");
+                System.out.print("Enter the base of your number (2, 8 or 10): ");
+                i--;
+            }
+        }
 
         System.out.print("Enter your number: ");
-        String number = s.nextLine();
-        int n = Integer.parseInt(number);
+        String number;
+        int n = 0;
+        boolean temp = false;
+        for (int i = 0; i < 1; i++) {
+            number = s.nextLine();
+            for (int j = 0; j < number.length()-1; j++) {
+                if (Integer.parseInt(String.valueOf(number.charAt(j))) < base){
+                    temp = true;
+                }
+            }
+            if (temp){
+                n = Integer.parseInt(number);
+            } else {
+                System.out.println("That's not valid!");
+                System.out.print("Enter your number: ");
+                i--;
+            }
+        }
 
         NumberConverter nc = new NumberConverter(n, base);
         int[] digits = nc.getDigits();
-        String temp = "";
+        String temp2 = "";
         System.out.println("\n\nDigit array: " + Arrays.toString(digits));
         System.out.println("Number: " + nc.displayNumber(nc.getDigits()));
         if (base != 2) {
@@ -29,9 +55,17 @@ class ConverterRunner {
         if (base != 10) {
             System.out.println("Decimal: " + nc.displayNumber(nc.convertToDecimal()));
         }
-        System.out.println("Additional base would you like to convert to (Only scales to base 64): ");
-        choice = s.nextLine();
-        base = Integer.parseInt(choice);
+        System.out.print("Additional base would you like to convert to (Only scales to base 64): ");
+        for (int i = 0; i < 1; i++) {
+            choice = s.nextLine();
+            if (Integer.parseInt(choice) <= 64){
+                base = Integer.parseInt(choice);
+            } else {
+                System.out.println("Have you considered that it only scales to 64");
+                System.out.print("Additional base would you like to convert to (Only scales to base 64): ");
+                i--;
+            }
+        }
         System.out.println("Base " + base + ": " + nc.displayNumber(nc.convertToAny(base)));
     }
 }
